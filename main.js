8a01119aff03d5f44e90fe6aff99b27eb2e8faa3
Agent 3 <agent.3@VENTIUS.LOCAL>
2023-02-01 15:19:43 +0100

    Initial Create of the project

---
+++ main.js
@@ -0,0 +1,57 @@
+const path = require('path')
+const { app, BrowserWindow, Menu } = require('electron');
+
+const isDev = process.env.NODE_ENV === 'development';
+const isMac = process.platfom === 'darwin'
+
+//Create the main Window
+function createMainWindow() {
+    const mainWindow = new BrowserWindow({
+        title: "Image Resizer",
+        width: isDev ? 1000 : 500,
+        height: 600,
+    });
+
+    //Open devtools if in dev env
+    if (isDev) {
+        mainWindow.webContents.openDevTools();
+    }
+
+    mainWindow.loadFile(path.join(__dirname, './renderer/index.html'));
+}
+
+
+//App is ready
+app.whenReady().then(() => {
+    createMainWindow();
+
+    //Implement menu
+    const mainMenu = Menu.buildFromTemplate(menu);
+    Menu.setApplicationMenu(mainMenu);
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createMainWindow();
+        }
+    })
+});
+
+//Menu template
+const menu = [
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'Quit',
+                click: () => app.quit(),
+                accelerator: 'CmdOrCtrl+W',
+            }
+        ]   
+    }
+];
+
+app.on('window-all-closed', () => {
+    if (!isMac) {
+        app.quit()
+    }
+})
